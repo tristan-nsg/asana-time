3e1f6be731170f2b21c438afb0399ed965cb1117
Tristan Isham <tristan@isham.co>
2026-03-04 16:16:35 -0500

    server

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,9 +1,11 @@
 import { Hono } from "@hono/hono";
+import { logger } from "@hono/hono/logger";
 
 if (import.meta.main) {
   const app = new Hono();
+  app.use(logger());
 
   app.get("/", (c) => c.text("Hono!"));
 
-  Deno.serve(app.fetch)
+  Deno.serve(app.fetch);
 }
